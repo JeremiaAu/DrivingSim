1074e7818115361443d96bd19d98a42b87b8bcc5
Jeremia Aulehla <jeremia.aulehla.github@protonmail.com>
2024-02-24 14:07:38 +0100

    Man kann jetzt Gas geben

--- gevp/scripts/vehicle_controllergd.gd
+++ gevp/scripts/vehicle_controllergd.gd
@@ -5,7 +5,7 @@ extends Node3D
 func _physics_process(delta):
 	vehicle_node.brake_input = Input.get_action_strength("Brakes")
 	vehicle_node.steering_input = Input.get_action_strength("Steer Left") - Input.get_action_strength("Steer Right")
-	vehicle_node.throttle_input = pow(Input.get_action_strength("Throttle"), 2.0)
+	vehicle_node.throttle_input = pow(getThrottle(), 2.0)
 	vehicle_node.handbrake_input = Input.get_action_strength("Handbrake")
 	vehicle_node.clutch_input = clampf(Input.get_action_strength("Clutch") + Input.get_action_strength("Handbrake"), 0.0, 1.0)
 	
@@ -19,5 +19,30 @@ func _physics_process(delta):
 		vehicle_node.manual_shift(-1)
 	
 	if vehicle_node.current_gear == -1:
-		vehicle_node.brake_input = Input.get_action_strength("Throttle")
+		vehicle_node.brake_input = getThrottle()
 		vehicle_node.throttle_input = Input.get_action_strength("Brakes")
+	
+var throttleControllerHasBeenUsed = false
+func getThrottle():
+	var keyboardInput = Input.get_action_strength("Throttle")
+	if keyboardInput == 1:
+		return keyboardInput
+	
+	var controllerInputUpperUnmodified = Input.get_action_strength("ThrottleUpperHalfInverse")
+	
+	if controllerInputUpperUnmodified != 0:
+		throttleControllerHasBeenUsed = true
+	
+	var controllerInputUpper = (controllerInputUpperUnmodified * -1) + 1
+	if !throttleControllerHasBeenUsed:
+		controllerInputUpper = 0
+		
+	var controllerInputLower = Input.get_action_strength("ThrottleLowerHalf")
+	var controllerInput = (controllerInputUpper + controllerInputLower) / 2
+	
+	print_debug("ThrottleUpper: " + str(controllerInputUpper))
+	print_debug("ThrottleLower: " + str(controllerInputLower))
+	print_debug("Throttle: " + str(controllerInput))
+	
+	return controllerInput
+	
